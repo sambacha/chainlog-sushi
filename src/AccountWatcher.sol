1d0444303edaa10ac63bcc0ddc2f35e7edb12da1
sam bacha <sam@manifoldfinance.com>
2022-05-08 19:26:08 -0700

    fix(lint): fmt

--- src/AccountWatcher.sol
+++ src/AccountWatcher.sol
@@ -1,38 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-
 struct Account {
     uint256 etherBalance;
     TokenBalance[] tokenBalances;
 }
-
 
 struct TokenBalance {
     bool callSuccess;
     uint256 balance;
 }
 
-
 interface ERC20Interface {
     function balanceOf(address account) external view returns (uint256 balance);
 }
 
-
 interface AccountWatcherInterface {
-    function balancesOf(
-        ERC20Interface[] calldata tokens, address[] calldata accounts
-    ) external view returns (Account[] memory accountBalances);
+    function balancesOf(ERC20Interface[] calldata tokens, address[] calldata accounts)
+        external
+        view
+        returns (Account[] memory accountBalances);
 }
-
 
 /// Quickly check the Ether balance, as well as the balance of each
 /// supplied ERC20 token, for a collection of accounts.
 /// @author 0age
 contract AccountWatcher is AccountWatcherInterface {
-    function balancesOf(
-        ERC20Interface[] calldata tokens, address[] calldata accounts
-    ) external view override returns (Account[] memory) {
+    function balancesOf(ERC20Interface[] calldata tokens, address[] calldata accounts)
+        external
+        view
+        override
+        returns (Account[] memory)
+    {
         Account[] memory accountBalances = new Account[](accounts.length);
 
         for (uint256 i = 0; i < accounts.length; i++) {
--- src/WalletSummarizer.sol
+++ src/WalletSummarizer.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-
 struct Account {
     uint256 etherBalance;
     TokenSummary[] tokenSummaries;
 }
 
-
 struct TokenQuery {
     ERC20Interface token;
     address[] spenders;
 }
-
 
 struct TokenSummary {
     bool balanceCheckSuccess;
@@ -20,37 +17,40 @@ struct TokenSummary {
     AllowanceCheck[] allowances;
 }
 
-
 struct AllowanceCheck {
     bool allowanceCheckSuccess;
     uint256 allowance;
 }
 
-
 interface ERC20Interface {
     function balanceOf(address account) external view returns (uint256);
+
     function allowance(address owner, address spender) external view returns (uint256);
 }
 
-
 interface WalletSummarizerInterface {
-    function summarize(TokenQuery[] calldata tokenQueries) external view returns (
-        Account memory accountSummary
-    );
-    function summarizeAccounts(
-        TokenQuery[] calldata tokenQueries, address[] calldata accounts
-    ) external view returns (Account[] memory accountSummaries);
-}
+    function summarize(TokenQuery[] calldata tokenQueries)
+        external
+        view
+        returns (Account memory accountSummary);
 
+    function summarizeAccounts(TokenQuery[] calldata tokenQueries, address[] calldata accounts)
+        external
+        view
+        returns (Account[] memory accountSummaries);
+}
 
 /// Quickly check the Ether balance, as well as the balance of each
 /// supplied ERC20 token and a set of approved allowances, for an account or a
 /// collection of accounts.
 /// @author 0age
 contract WalletSummarizer is WalletSummarizerInterface {
-    function summarize(
-        TokenQuery[] calldata tokenQueries
-    ) external view override returns (Account memory) {
+    function summarize(TokenQuery[] calldata tokenQueries)
+        external
+        view
+        override
+        returns (Account memory)
+    {
         Account memory accountSummary;
         bool success;
         bytes memory returnData;
@@ -61,7 +61,7 @@ contract WalletSummarizer is WalletSummarizerInterface {
             TokenSummary memory tokenSummary;
             TokenQuery memory tokenQuery = tokenQueries[i];
             ERC20Interface token = tokenQuery.token;
-            (success, returnData) = address(token).staticcall{gas: gasleft() / 4}(
+            (success, returnData) = address(token).staticcall{ gas: gasleft() / 4 }(
                 abi.encodeWithSelector(token.balanceOf.selector, msg.sender)
             );
 
@@ -75,7 +75,7 @@ contract WalletSummarizer is WalletSummarizerInterface {
             for (uint256 j = 0; j < spenders.length; j++) {
                 AllowanceCheck memory allowanceCheck;
                 address spender = spenders[j];
-                (success, returnData) = address(token).staticcall{gas: gasleft() / 4}(
+                (success, returnData) = address(token).staticcall{ gas: gasleft() / 4 }(
                     abi.encodeWithSelector(token.allowance.selector, msg.sender, spender)
                 );
 
@@ -97,9 +97,12 @@ contract WalletSummarizer is WalletSummarizerInterface {
         return accountSummary;
     }
 
-    function summarizeAccounts(
-        TokenQuery[] calldata tokenQueries, address[] calldata accounts
-    ) external view override returns (Account[] memory) {
+    function summarizeAccounts(TokenQuery[] calldata tokenQueries, address[] calldata accounts)
+        external
+        view
+        override
+        returns (Account[] memory)
+    {
         Account[] memory accountSummaries = new Account[](accounts.length);
 
         bool success;
@@ -113,7 +116,7 @@ contract WalletSummarizer is WalletSummarizerInterface {
                 TokenSummary memory tokenSummary;
                 TokenQuery memory tokenQuery = tokenQueries[j];
                 ERC20Interface token = tokenQuery.token;
-                (success, returnData) = address(token).staticcall{gas: gasleft() / 4}(
+                (success, returnData) = address(token).staticcall{ gas: gasleft() / 4 }(
                     abi.encodeWithSelector(token.balanceOf.selector, account)
                 );
 
@@ -127,7 +130,7 @@ contract WalletSummarizer is WalletSummarizerInterface {
                 for (uint256 k = 0; k < spenders.length; k++) {
                     AllowanceCheck memory allowanceCheck;
                     address spender = spenders[k];
-                    (success, returnData) = address(token).staticcall{gas: gasleft() / 4}(
+                    (success, returnData) = address(token).staticcall{ gas: gasleft() / 4 }(
                         abi.encodeWithSelector(token.allowance.selector, account, spender)
                     );
 
