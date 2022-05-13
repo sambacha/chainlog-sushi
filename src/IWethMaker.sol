238674bfcd53bbb24d92dc8f46a8ee3a80e22b9c
sam bacha <sam@manifoldfinance.com>
2022-05-13 13:43:55 -0700

    fix: pragma

--- src/IWethMaker.sol
+++ src/IWethMaker.sol
@@ -1,5 +1,5 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
+/// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
 interface IWethMaker {
     event SetTrusted(address indexed user, bool isTrusted);
